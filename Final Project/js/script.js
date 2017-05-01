65233f93cb3a6a821e80a8e58761bfc463de0cac
Bryan_S <bryansaylor1@gmail.com>
2017-05-01 14:31:20 -0700

    revision

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -6,22 +6,18 @@ $(".read-less").click(hideStuff);
 $(".music-pic").click(doModalStuff);
 $("#modal").click(clearModal);
 
-
-function showStuff(){
-	event.preventDefault();
-
-	// $(this).find("div").slideDown("fast");
-	$(".show-this-on-click").slideDown("fast");
-	$(".read-more").hide();
-	$(".read-less").show();
+function showStuff(e) {
+	e.preventDefault();
+	$(this).siblings(".hide").first().slideDown();
+	$(this).siblings(".read-less").first().show();
+	$(this).hide();
 }
 
-function hideStuff(){
-	event.preventDefault();
-	$(".show-this-on-click").slideUp("fast", function(){
-		$(".read-less").hide();
-		$(".read-more").show();
-	});
+function hideStuff(e) {
+	e.preventDefault();
+	$(this).prevAll(".show-this-on-click").last().slideUp();
+	$(this).prevAll(".read-more").last().show();
+	$(this).hide();
 }
 
 function doModalStuff() {
@@ -35,4 +31,30 @@ function clearModal() {
 	$("#modal").empty();
 }
 
+// function showStuff(){
+// 	event.preventDefault();
+
+// 	// $(this).find("div").slideDown("fast");
+// 	$(".show-this-on-click").slideDown("fast");
+// 	$(".read-more").hide();
+// 	$(".read-less").show();
+// }
+
+// function showStuff(e){
+//   e.preventDefault();
+//   $(this).siblings('.show-this-on-click').slideDown();
+//   $(this).hide();
+//   $(this).siblings(".read-less").show();
+// }
+
+// function hideStuff(e){
+// 	e.preventDefault();
+// 	$(this).prev().slideUp();
+// 	$(this).hide();
+// 	$(this).prev().prev().show();
+	// $(".show-this-on-click").slideUp("fast", function(){
+	// 	$(".read-less").hide();
+	// 	$(".read-more").show();
+	// });
+// }
 });
