6258547e3aea97eb5e581388a6934e39829c3b1c
Bryan_S <bryansaylor1@gmail.com>
2017-05-17 12:36:08 -0700

    added commnts to js

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -10,17 +10,16 @@ $(document).on('keyup',function(evt) {
        clearModal(); //for my wife
     }
 });
-var scroll = 0;
 
 
-function showStuff(e) {
+function showStuff(e) { //shows hidden text content and hides 'show more' button and shows read less button
 	e.preventDefault();
 	$(this).siblings(".hide").first().slideDown("fast");
 	$(this).siblings(".read-less").first().show();
 	$(this).hide();
 }
 
-function hideStuff(e) {
+function hideStuff(e) { //hides text from above and read less btton and shows show more button
 	e.preventDefault();
 	$(this).prevAll(".show-this-on-click").last().slideUp("fast", function(){
 		$(this).prevAll(".read-more").last().show();
@@ -28,19 +27,19 @@ function hideStuff(e) {
 	$(this).hide();
 }
 
-function doModalStuff() {
-	var picUrl = $(this).attr("src");
+function doModalStuff() { //activates modal to 'zoom' clicked picture
+	var picUrl = $(this).attr("src"); //gets specific url of picture clicked
 	$("#modal").addClass("modal-active");
 	$("#modal").append("<img class='music-pic big-pic' src="+picUrl+">");
 }
 
-function clearModal() {
+function clearModal() { //clears modal class and emptird appended html from above
 	$("#modal").removeClass("modal-active");
 	$("#modal").empty();
 }
 
 
-$("nav ul li:first-child").click(function(){
+$("nav ul li:first-child").click(function(){ //adds 'show' class to nav for mobile
 	console.log("hamburger clicked");
 	$("nav").toggleClass("show");
 	});
